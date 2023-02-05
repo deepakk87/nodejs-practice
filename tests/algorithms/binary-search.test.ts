17733abd664323ce8da7064eea73c00e42e3695e
Deepak Kumar <deepakk87@gmail.com>
2023-02-05 12:39:00 -0800

    Better Version of binary search

--- tests/algorithms/binary-search.test.ts
+++ tests/algorithms/binary-search.test.ts
@@ -1,120 +1,59 @@
-function binarySearch(
-  nums: number[],
-  start: number,
-  end: number,
-  target: number,
-) {
-  if (end - start <= 0) return -1;
-
-  const mid = Math.floor((start + end) / 2);
-
-  console.log(mid);
-
-  if (nums[mid] == target) return mid;
-  // left sorted
-  if (nums[start] <= nums[mid]) {
-    if (target > nums[mid] || target < nums[start]) {
-      return binarySearch(nums, mid + 1, end, target);
-    } else {
-      return binarySearch(nums, start, mid, target);
-    }
-  } else {
-    // right sorted
-    if (target < nums[mid] || target > nums[end - 1]) {
-      return binarySearch(nums, start, mid, target);
-    } else {
-      return binarySearch(nums, mid + 1, end, target);
-    }
-  }
-}
-
-function search(nums: number[], target: number): number {
-  return binarySearch(nums, 0, nums.length, target);
-}
-
+// Simple binary search
+// https://leetcode.com/problems/binary-search
+// https://leetcode.com/problems/search-insert-position/
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 function lowerBound(arr: number[], target: number) {
   let low = 0;
   let high = arr.length;
-  let index = -1;
-  while (low <= high) {
+  while (low < high) {
     const mid = Math.floor((low + high) / 2);
-    if (arr[mid] == target) {
-      index = mid;
-    }
-    console.log(`${low}, ${high}, ${mid}`);
-    if (target <= arr[mid]) {
+    console.log(`low = ${low}, high = ${high}, mid = ${mid}`);
+    if (arr[mid] < target) {
       // target >= arr
-      high = mid - 1;
-    } else {
       low = mid + 1;
+    } else {
+      high = mid;
     }
   }
-  return index;
+  return low;
 }
 
 function upperBound(arr: number[], target: number) {
   let low = 0;
   let high = arr.length;
-  let index = -1;
-  while (low <= high) {
+  while (low < high) {
     const mid = Math.floor((low + high) / 2);
-    if (arr[mid] == target) {
-      index = mid;
-    }
-    console.log(`${low}, ${high}, ${mid}`);
-    if (target >= arr[mid]) {
+    // console.log(`${low}, ${high}, ${mid}`);
+    if (!(target < arr[mid])) {
       // target >= arr
       low = mid + 1;
     } else {
-      high = mid - 1;
+      high = mid;
     }
   }
-  return index;
+  return high;
 }
 
-function searchInsert(nums: number[], target: number): number {
-  let low = 0;
-  let high = nums.length;
-  let index = low;
-  while (low <= high) {
-    const mid = Math.floor((low + high) / 2);
-    console.log(`${low}, ${high}, ${mid}`);
-    index = low;
-    if (target <= nums[mid]) {
-      // target >= arr
-      high = mid - 1;
-    } else {
-      low = mid + 1;
-    }
-  }
-  return index;
+function searchRange(nums: number[], target: number): number[] {
+  let low = lowerBound(nums, target);
+  let up = upperBound(nums, target);
+
+  low = nums[low] == target ? low : -1;
+  up = nums[up - 1] == target ? up - 1 : -1;
+  return [low, up];
 }
 
-describe('Simple binary search on Rotated Array Test', () => {
+function search(nums: number[], target: number): number {
+  const index = lowerBound(nums, target);
+  return nums[index] == target ? index : -1;
+}
+
+describe('Simple Test', () => {
   it('test that  works', async () => {
-    expect(search([3, 1], 3)).toBe(0);
+    expect(search([2, 5], 5)).toBe(1);
   });
 
   it('test that  works 2', async () => {
-    expect(search([4, 5, 6, 7, 0, 1, 2], 0)).toBe(4);
-  });
-
-  it('test that lowerbound/upperbound works 1', async () => {
-    expect(lowerBound([1, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4], 3)).toBe(1);
-    expect(upperBound([1, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4], 3)).toBe(6);
-  });
-
-  it('test that lowerBound/upperbound works 2', async () => {
-    expect(lowerBound([1, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4], 2)).toBe(-1);
-    expect(upperBound([1, 1, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4], 2)).toBe(-1);
-  });
-
-  it('test that lowerBound/upperbound works 3', async () => {
-    expect(upperBound([5, 7, 7, 8, 8, 10], 8)).toBe(4);
-    expect(lowerBound([5, 7, 7, 8, 8, 10], 8)).toBe(3);
-  });
-
-  it('test that search insert works', async () => {
-    expect(searchInsert([1, 3, 5, 6], 7)).toBe(4);
+    expect(searchRange([5, 7, 7, 8, 8, 10], 8)).toEqual([3, 4]);
   });
 });
